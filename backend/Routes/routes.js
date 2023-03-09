cd44647ec13ecd8a24e4babf4d1909eb147d46c1
zar7an <akashkishore068@gmail.com>
2023-03-09 20:42:35 +0530

    made changes

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const quoteModel = require('../Models/model')
-const connectDb = require('../utils/db')
 const route = express.Router()
 
 
 route.get('/quotes', async (req, res) => {
   try {
-    connectDb()
     const quote = await quoteModel.aggregate([{ $sample: { size: 1 } }])
     res.status(200).send({res: quote[0].quoteText})
   }catch(e) {
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const home = require('./Routes/routes')
+const connectDb = require('./utils/db')
 require('dotenv').config()
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/v1', home)
+connectDb();
 
 app.listen(process.env.PORT || 4000, () => console.log(`server is listening at ${process.env.PORT}`))
